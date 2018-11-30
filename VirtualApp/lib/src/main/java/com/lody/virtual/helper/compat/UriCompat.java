b0330c3ab16bfb895f982144656f07df7a2ef51d
guozhenxiang <zhenxiang.guo@singuloid.com>
2018-11-30 14:23:38 +0800

    完善VA

--- VirtualApp/lib/src/main/java/com/lody/virtual/helper/compat/UriCompat.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/helper/compat/UriCompat.java
@@ -20,7 +20,8 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 public class UriCompat {
-    public static String AUTH = "virtual.fileprovider";
+    //    public static String AUTH = "virtual.fileprovider";
+    public static String AUTH = "secspace.fileprovider";
     private static boolean DEBUG = false;
     private static final String TAG = "UriCompat";
 
